7e9819de7bfed7c844eb162eab5e77c162e66a6c
Ada Lee <lili545442483@163.com>
2016-01-27 22:51:35 +0800

    use highline gem to create user's own UdaciList

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'chronic'
 require 'colorize'
 # Find a third gem of your choice and add it to your project
 require 'terminal-table'
+require 'highline'
 
 require_relative "lib/listable"
 require_relative "lib/errors"
@@ -46,3 +47,39 @@ new_list.all
 # DEMO FILTER BY ITEM TYPE
 # ------------------------
 puts new_list.filter("event")
+
+
+# User input here
+cli = HighLine.new
+answer = cli.ask "Do you want to have your own UdaciList? (yes/no)"
+if answer.start_with? "y"
+  user_list = UdaciList.new(title: "Your Stuff")
+  puts "You can add three kind of item: event, link, todo."
+  puts "Format is shown below\n"
+  puts "event|description here|start date here|end date here"
+  puts "link|url here|sitename here"
+  puts "todo|description here|due date here|priority here (high, medium, low supported)"
+  puts "\nif you have accomplished, type end"
+  while true
+    answer = cli.ask("\nAdd items to UdaciList, type end to quit.")
+    if answer == "end"
+      break
+    end
+    tokens = answer.split("|")
+    if tokens[0] == "event"
+      _, description, start_date, end_date = tokens
+      user_list.add("event", description, start_date: start_date, end_date: end_date)
+    elsif tokens[0] == "link"
+      _, url, site_name = tokens
+      user_list.add("link", url, site_name: site_name)
+    elsif tokens[0] == 'todo'
+      _, description, due, priority = tokens
+      user_list.add("todo", description, due: due, priority: priority)
+    else
+      puts "wrong format"
+      break
+    end
+  end
+  user_list.all
+end
+
