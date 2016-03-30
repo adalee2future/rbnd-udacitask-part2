291ea6f9842b12607702169b0a68af6a99fe88de
ada.li <ada.li@alo7.com>
2016-03-30 18:53:51 +0800

    add EventPeriodError

--- lib/event.rb
+++ lib/event.rb
@@ -6,6 +6,9 @@ class EventItem
     @description = description
     @start_date = Chronic.parse(options[:start_date]) if options[:start_date]
     @end_date = Chronic.parse(options[:end_date]) if options[:end_date]
+    if @start_date && @end_date && (@start_date > @end_date)
+      raise UdaciListErrors::EventPeriodError, "start date should be before end date!"
+    end
   end
   def format_date
     dates = @start_date.strftime("%D") if @start_date
